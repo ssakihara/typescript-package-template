05fdbdc720b84a14be88ee87b4ef95ebbf8a327f
ssakihara <sakihara.shinnosuke@gmail.com>
2023-08-30 14:00:30 +0900

    feat: 🎸 add logger

---
+++ src/core/logging/logger.ts
@@ -0,0 +1,50 @@
+import log4js, { Level, LoggingEvent } from 'log4js';
+
+const level = process.env.LOG_LEVEL ?? 'info';
+const appenders = process.env.K_SERVICE ? ['gcp'] : ['console'];
+
+const gcpAppender = (logEvent: LoggingEvent) => {
+  const entry: {
+    errorMessage?: Error;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jsonPayload?: any[];
+    message?: string;
+    severity: Level;
+  } = {
+    severity: logEvent.level ?? 'DEBUG',
+  };
+
+  if (logEvent?.error) {
+    entry.errorMessage = logEvent?.error;
+  } else if (typeof logEvent?.data?.[0] == 'string' && logEvent?.data.length === 1) {
+    entry.message = logEvent?.data?.[0];
+  } else if (logEvent?.data.length === 1) {
+    entry.jsonPayload = { ...logEvent?.data[0] };
+  } else {
+    entry.jsonPayload = logEvent?.data;
+  }
+
+  console.log(entry);
+};
+
+log4js.configure({
+  appenders: {
+    console: {
+      type: 'console',
+      layout: { type: 'pattern', pattern: '%[%m%]' },
+    },
+    gcp: {
+      type: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        configure: (config, layouts, findAppender, levels) => {
+          return gcpAppender;
+        },
+      },
+    },
+  },
+  categories: { default: { appenders, level } },
+});
+
+const logger = log4js.getLogger();
+
+export { logger };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { logger } from '@/core/logging/logger.js';
+
+export { logger } from '@/core/logging/logger.js';
 export const helloWorld = () => {
-  console.log('Hello World !!');
+  logger.debug('Hello World !!');
 };
